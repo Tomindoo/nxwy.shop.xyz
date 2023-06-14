51c9b4d430fc76780450e89133ac77c6d3ef2286
Tomindoo <130087360+Tomindoo@users.noreply.github.com>
2023-06-14 17:51:51 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+body {
+  background-color: #000000;
+  color: #ffffff;
+  font-family: Arial, sans-serif;
+}
+
+#canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+
+#image-container {
+  margin-top: 20px;
+  text-align: center;
+}
+
+#image-container img {
+  max-width: 100%;
+  height: auto;
+}
+
+#button-container {
+  text-align: center;
+  margin-top: 20px;
+}
+
+#my-button {
+  background-color: #ffffff;
+  color: #000000;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+}
